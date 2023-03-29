c9e7d3350349455a2065ebc2634f20353805bd0b
hoang.danh.quan <hoang.danh.quan@sun-asterisk.com>
2023-03-29 23:29:45 +0700

    ep 10

---
+++ ep 10/main.js
@@ -0,0 +1,109 @@
+window.onload = function(){
+    var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight,
+    ship = particle.create(width/2, height/2, 0, 0,0),
+    thrush = vector.create(0, 0),
+    angle = 0,
+    turningLeft = false,
+    turningRight = false,
+    thrusting = false;
+
+    update();
+
+    document.body.addEventListener("keydown", function(event){
+        console.log(event.keyCode);
+        switch (event.keyCode){
+            case 38: // up
+                thrusting = true;
+                break;
+            case 37: // left
+                turningLeft = true;
+                break;
+            case 39: // right
+                turningRight = true;
+                break;
+            default:
+                break;
+        }
+    });
+
+    document.body.addEventListener("keyup", function(event){
+        // console.log(event.keyCode);
+        switch (event.keyCode){
+            case 38: // up
+                thrusting = false;
+                break;
+            case 37: // left
+                turningLeft = false;
+                break;
+            case 39: // right
+                turningRight = false;
+                break;
+            default:
+                break;
+        }
+    });
+    
+    function update(){
+        context.clearRect(0, 0, width, height);
+        
+        if (turningLeft){
+            angle -= 0.05;
+        }
+        if (turningRight){
+            angle += 0.05;
+        }
+
+        thrush.setAngle(angle);
+        if (thrusting){
+            thrush.setLength(0.1);
+        }else{
+            thrush.setLength(0);
+        }
+
+        ship.accelerate(thrush);
+        ship.update();
+
+        context.save();
+        context.translate(ship.position.getX(), ship.position.getY());
+        context.rotate(angle);
+
+        context.beginPath();
+        context.moveTo(10, 0);
+        context.lineTo(-10, -7);
+        context.lineTo(-10, 7);
+        context.lineTo(10, 0);
+        if (thrusting){
+            context.moveTo(-10, 0);
+            context.lineTo(-18, 0);
+        }
+        context.stroke();
+        context.restore();
+
+        // context.beginPath();
+        // context.arc(ship.position.getX(), ship.position.getY(), 10,
+        //             0, Math.PI *2, false);
+        // context.fill();
+        
+        if (ship.position.getX() > width){
+            ship.position.setX(0);
+        }
+
+        if (ship.position.getX() < 0){
+            ship.position.setX(width);
+        }
+
+        if (ship.position.getY() > height){
+            ship.position.setY(0);
+        }
+
+        if (ship.position.getY() < 0){
+            ship.position.setY(height);
+        }
+        
+        requestAnimationFrame(update);
+    }
+
+}
