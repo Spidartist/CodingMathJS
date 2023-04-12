f714dccad2f86d8b4f9d69a13c5a06aa69f152f1
Spidartist <hoangdanhquan48@gmail.com>
2023-04-12 08:34:33 +0700

    mini7

--- mini7/main.js
+++ mini7/main.js
@@ -2,27 +2,20 @@ window.onload = function(){
     var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
     width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight,
-    centerX = width/2,
-    centerY = height/2;
+    height = canvas.height = window.innerHeight;
 
-    function distanceXY(x0, y0, x1, y1){
-        var dx = x1 - x0,
-            dy = y1 - y0;
-        return Math.sqrt(dx*dx + dy*dy);
-    }
+    var angle = Math.PI / 4;
 
-    document.body.addEventListener("mousemove", function(event){
-        context.clearRect(0, 0, width, height);
+    context.translate(width/2, height/2);
+    context.rotate(angle);
 
-        var dist = distanceXY(event.clientX, event.clientY, centerX, centerY);
-        if (dist < 100){
-            context.fillStyle = "#ff6666"
-        }else{
-            context.fillStyle = "#cccccc";
-        } 
-        context.beginPath();
-        context.arc(centerX, centerY, 100, 0, Math.PI * 2, false);
-        context.fill();
-    })
-}
+    context.beginPath()
+    context.arc(0, 0, 20, 0 , Math.PI * 2, false);
+    context.fill();
+
+    context.lineWidth = 10;
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(50, 0);
+    context.stroke();
+};
