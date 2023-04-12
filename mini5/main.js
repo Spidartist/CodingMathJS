282b1c8d0db83f24f50d2582f1d26f6ecbde7b60
Spidartist <hoangdanhquan48@gmail.com>
2023-04-12 08:04:08 +0700

    mini5

--- mini5/main.js
+++ mini5/main.js
@@ -2,16 +2,27 @@ window.onload = function(){
     var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
     width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight;
+    height = canvas.height = window.innerHeight,
+    centerX = width/2,
+    centerY = height/2;
 
-    update();
-
-    function update(){
-        context.clearRect(0, 0, width, height);
-
-        //
-
-        requestAnimationFrame(update);
+    function distanceXY(x0, y0, x1, y1){
+        var dx = x1 - x0,
+            dy = y1 - y0;
+        return Math.sqrt(dx*dx + dy*dy);
     }
 
+    document.body.addEventListener("mousemove", function(event){
+        context.clearRect(0, 0, width, height);
+
+        var dist = distanceXY(event.clientX, event.clientY, centerX, centerY);
+        if (dist < 100){
+            context.fillStyle = "#ff6666"
+        }else{
+            context.fillStyle = "#cccccc";
+        } 
+        context.beginPath();
+        context.arc(centerX, centerY, 100, 0, Math.PI * 2, false);
+        context.fill();
+    })
 }
