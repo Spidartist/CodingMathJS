820aaffc128cfb4158e4966c0d3005b9a2457f0c
Spidartist <hoangdanhquan48@gmail.com>
2023-04-25 13:16:24 +0700

    done 26

---
+++ ep26/2d.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight,
+        fl = 300,
+        point = {
+            x: 200,
+            y: 300
+        },
+        delta = 0.05;
+        
+    context.translate(width/2, height/2);
+
+    update();
+
+    function update(){
+        context.clearRect(-width/2, -height/2, width, height);
+
+        context.beginPath()
+        context.arc(point.x, point.y, 20, 0, Math.PI*2, false);
+        context.fill()
+
+        var cos = Math.cos(delta),
+            sin = Math.sin(delta),
+            x = point.x * cos - point.y * sin,
+            y = point.x * sin + point.y * cos;
+        
+        point.x = x;
+        point.y = y;
+        
+
+        requestAnimationFrame(update);
+    }
+
+}
