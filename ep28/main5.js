54e1079afb118fe51085cd15bbfa446d6f2e8f0e
Spidartist <hoangdanhquan48@gmail.com>
2023-04-27 17:48:38 +0700

    worm game

--- ep28/main5.js
+++ ep28/main5.js
@@ -8,14 +8,21 @@ window.onload = function(){
         y: Math.random() * height
     },
     points = [],
-    numPoints = 100,
-    ease = 0.1;
+    numPoints = 30,
+    ease = 0.4;
 
     for (var i = 0;i<numPoints;i++){
-        points.push({
+        p = {
             x: 0,
-            y: 0
-        })
+            y: 0,
+            img: document.createElement("img")
+        };
+        if (i==0){
+            p.img.src = "./pixil-frame-0 (3).png"
+        }else{
+            p.img.src = "./pixil-frame-0 (4).png"
+        }
+        points.push(p);
     }
 
     update();
@@ -39,13 +46,22 @@ window.onload = function(){
             p.x += (leader.x - p.x) * ease;
             p.y += (leader.y - p.y) * ease;
 
-            context.beginPath();
-            context.arc(p.x, p.y, 10, 0, Math.PI * 2, false);
-            context.fill();
+            // context.drawImage(p.img, p.x, p.y);
 
             leader.x = p.x;
             leader.y = p.y;
         }
+
+        
+
+        for (var i=numPoints-1;i>=0;i--){
+            var p = points[i];
+            // context.save();
+            // context.scale(1.2, 1.2);
+            context.drawImage(p.img, p.x, p.y);
+            // context.restore();
+        }
+
         
         requestAnimationFrame(update);
     }
