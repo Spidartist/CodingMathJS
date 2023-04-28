97f6e715584a07f63ab86fd1853ae049a729269c
Spidartist <hoangdanhquan48@gmail.com>
2023-04-29 01:15:59 +0700

    dcm nham slice vs splice a fix ca chieu k xong dcm cuoc doi

--- Ship/ship.js
+++ Ship/ship.js
@@ -1,17 +1,17 @@
 window.onload = function(){
     var canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight,
-    ship = particle.create(width/2, height/2, 0, 0,0),
-    thrush = vector.create(0, 0),
-    bullets = [],
-    obstacles = [],
-    angle = 0,
-    turningLeft = false,
-    turningRight = false,
-    thrusting = false,
-    fire = false;
+        context = canvas.getContext("2d"),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight,
+        ship = particle.create(width/2, height/2, 0, 0,0),
+        thrush = vector.create(0, 0),
+        bullets = [],
+        obstacles = [],
+        angle = 0,
+        turningLeft = false,
+        turningRight = false,
+        thrusting = false,
+        fire = false;
 
     ship.friction = 0.99;
 
@@ -99,36 +99,7 @@ window.onload = function(){
 
         context.restore();
 
-        // if (Math.random() < 0.01){
-        //     var seed = utils.randomInt(1, 4);
-        //     switch (seed) {
-        //         case 1:
-        //             obstacles.push(obstacle.create(0,
-        //                             utils.randomRange(0, height),
-        //                             utils.randomRange(2, 5),
-        //                             utils.randomRange(0, Math.PI * 2)));
-        //             break;
-        //         case 2:
-        //             obstacles.push(obstacle.create(width,
-        //                                         utils.randomRange(0, height),
-        //                                         utils.randomRange(2, 5),
-        //                                         utils.randomRange(0, Math.PI * 2)));
-        //             break;
-        //         case 3:
-        //             obstacles.push(obstacle.create(utils.randomRange(0, width),
-        //                             0,
-        //                             utils.randomRange(2, 5),
-        //                             utils.randomRange(0, Math.PI * 2)));
-        //             break;
-        //         case 4:
-        //             obstacles.push(obstacle.create(utils.randomRange(0, width),
-        //                             height,
-        //                             utils.randomRange(2, 5),
-        //                             utils.randomRange(0, Math.PI * 2)));
-        //             break;           
-        //     }
-
-        // }
+        // genObstacle();
 
         for (var i = 0;i<obstacles.length;i++){
             var o = obstacles[i];
@@ -140,44 +111,11 @@ window.onload = function(){
             b.update();
         }
 
-        context.fillStyle = "red";
-        for (var i = bullets.length - 1; i>=0; i--){
-            var b = bullets[i];
-            if (b.x < 0 || b.x > width || b.y < 0 || b.y > height){
-                bullets.splice(i, 1);
-            }else{
-                context.beginPath();
-                context.arc(b.x, b.y, b.radius, 0, Math.PI * 2, false);
-                context.fill();
-            }
-        }
+        checkBoundary();
 
-        context.fillStyle = "green";
-        for (var i = obstacles.length - 1; i>=0; i--){
-            var o = obstacles[i];
-            if (o.x < 0 || o.x > width || o.y < 0 || o.y > height){
-                obstacles.splice(i, 1);
-            }else{
-                context.beginPath();
-                context.arc(o.x, o.y, o.radius, 0, Math.PI * 2, false);
-                context.fill();
-            }
-        }
+        checkCollision();
 
-        for (var j = bullets.length - 1; j>=0; j--){
-            var b = bullets[j];
-            for (var i = obstacles.length - 1; i>=0; i--){
-                var o = obstacles[i];
-                if (utils.circleCollision(o, b)){
-                    console.log(obstacles.length);
-                    obstacles.slice(i, 1);
-                    bullets.splice(j, 1);
-                }
-            }
-        }
-
-        
-        // Todo: seperate check boundary
+        draw();
 
         if (ship.x > width){
             ship.x = 0;
@@ -196,6 +134,103 @@ window.onload = function(){
         }
         
         requestAnimationFrame(update);
+    }
+
+    function checkBoundary(){
+        for (var i = bullets.length - 1; i>=0; i--){
+            var b = bullets[i];
+            if (b.x < 0 || b.x > width || b.y < 0 || b.y > height){
+                bullets.splice(i, 1);
+            }
+        }
+
+        for (var i = obstacles.length - 1; i>=0; i--){
+            var o = obstacles[i];
+            // if (o.x - o.radius > width || o.x + o.radius < 0
+            //     || o.y - o.radius > height || o.y + o.radius < 0){
+            //     obstacles.splice(i, 1);
+            // }
+            if (o.x - o.radius > width){
+                o.x = (-o.radius);
+            }
+            if (o.x + o.radius < 0){
+                o.x = (width + o.radius);
+            }
+            if (o.y - o.radius > height){
+                o.y = (-o.radius);
+            }
+            if (o.y + o.radius < 0){
+                o.y = (height + o.radius);
+            }
+        }
+    }
+
+    function checkCollision(){
+        for (var j = 0; j<bullets.length; j++){
+            var b = bullets[j];
+            for (var i = 0; i<obstacles.length; i++){
+                var o = obstacles[i];
+                if (utils.circleCollision(o, b)){
+                    // console.log(obstacles.length);
+                    obstacles.splice(i, 1);
+                    console.log("a" + bullets.length);
+                    bullets.splice(j, 1);
+                    console.log(bullets.length);
+
+                }
+            }
+        }
+    }
+
+    function draw(){
+        context.fillStyle = "red";
+        for (var i = bullets.length - 1; i>=0; i--){
+            var b = bullets[i];
+            context.beginPath();
+            context.arc(b.x, b.y, b.radius, 0, Math.PI * 2, false);
+            context.fill();
+        }
+
+        context.fillStyle = "green";
+        for (var i = obstacles.length - 1; i>=0; i--){
+            var o = obstacles[i];
+            context.beginPath();
+            context.arc(o.x, o.y, o.radius, 0, Math.PI * 2, false);
+            context.fill();
+        }
+    }
+
+    function genObstacle(){
+        if (Math.random() < 0.1){
+            var seed = utils.randomInt(1, 4);
+            switch (seed) {
+                case 1:
+                    obstacles.push(obstacle.create(0,
+                                    utils.randomRange(0, height),
+                                    utils.randomRange(2, 5),
+                                    utils.randomRange(0, Math.PI * 2)));
+                    break;
+                case 2:
+                    obstacles.push(obstacle.create(width,
+                                                utils.randomRange(0, height),
+                                                utils.randomRange(2, 5),
+                                                utils.randomRange(0, Math.PI * 2)));
+                    break;
+                case 3:
+                    obstacles.push(obstacle.create(utils.randomRange(0, width),
+                                    0,
+                                    utils.randomRange(2, 5),
+                                    utils.randomRange(0, Math.PI * 2)));
+                    break;
+                case 4:
+                    obstacles.push(obstacle.create(utils.randomRange(0, width),
+                                    height,
+                                    utils.randomRange(2, 5),
+                                    utils.randomRange(0, Math.PI * 2)));
+                    break;           
+            }
+
+        }
     }
 
 }
